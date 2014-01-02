1345412560d8e53d45c53dd3a632406e8dbf477f
kewelbeanz <kewelbeanz@yahoo.com>
2014-01-02 01:28:22 -0500

    iteration 3

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -10,7 +10,7 @@ module ProductsHelper
 
     def print_stock(stock)
     	if stock > 0
-        content_tag(:span, "In Stock", class:"in_stock")        
+        content_tag(:span, "In Stock ("+stock.to_s+")", class:"in_stock")        
       else
         content_tag(:span, "Out of Stock", class:"out_stock")    	  
     	end
---
+++ app/models/order_item.rb
@@ -0,0 +1,5 @@
+class OrderItem < ActiveRecord::Base
+	belongs_to :order
+	belongs_to :product
+	validates_presence_of :order_id, :product_id
+end
