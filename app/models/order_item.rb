5355e5ce7f9fdac76e403e8bdd3dbaaf4836a114
kewelbeanz <kewelbeanz@yahoo.com>
2014-01-05 21:19:59 -0500

    Iteration 4: Orders

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,4 +2,11 @@ class OrderItem < ActiveRecord::Base
 	belongs_to :order
 	belongs_to :product
 	validates_presence_of :order_id, :product_id
+
+	def subtotal
+		quantity*product.price
+	end
+
+		
+		
 end
