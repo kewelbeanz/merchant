da1262890554dd1e8adab3802b5c66f5103599b2
kewelbeanz <kewelbeanz@yahoo.com>
2014-01-12 14:33:37 -0500

    Changed to my Twitter info

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "i0KU4jLYYjWzxpTraWviw", "djMu7QStnBK89MkfIg78tIZ4sFhmGOYjdCjDD0Wsc"
+  provider :twitter, "8DDtAwYPgMS3eOLb4Q9Fw","31098784-RpRPDTy6M3sZzTdYdLJKrGG0WDHW7X6tx7CYthp1r"#"i0KU4jLYYjWzxpTraWviw", "djMu7QStnBK89MkfIg78tIZ4sFhmGOYjdCjDD0Wsc"
 end
